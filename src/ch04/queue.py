036d07b6aff79a3c9b47d0918bbfe47869b08c8e
Benjamin Blumenfeld-Jones <BensThoughts@Gmail.com>
2021-08-12 00:42:30 -0700

    basic FIFO Queue implemented

--- src/ch04/queue.py
+++ src/ch04/queue.py
@@ -2,3 +2,26 @@ class Node:
   def __init__(self, val) -> None:
       self.value = val
       self.next = None
+
+class Queue:
+  def __init__(self) -> None:
+      self.first = None
+      self.last = None
+  
+  def is_empy(self):
+    return self.first is None
+
+  def enqueue(self, val):
+    if self.first is None:
+      self.first = self.last = Node(val)
+    else:
+      self.last.next = Node(val)
+      self.last = self.last.next
+
+  def dequeue(self):
+    if self.is_empy():
+      raise RuntimeError('Queue is empty.')
+
+    val = self.first.value
+    self.first = self.first.next
+    return val
